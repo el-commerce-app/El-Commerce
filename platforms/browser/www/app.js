c8a0151eb52f6efb878552607a6fac936a5432a5
Ivan Mikhailov <yar-dezing@ya.ru>
2016-11-27 17:08:42 +0300

    Start

---
+++ platforms/browser/www/app.js
@@ -0,0 +1,35 @@
+require.config({
+    paths: {
+        handlebars: "lib/handlebars",
+        text: "lib/text",
+        hbs: "lib/hbs"
+    },
+    shim: {
+        handlebars: {
+            exports: "Handlebars"
+        }
+    }
+});
+define('app', ['js/router', 'js/utils'], function(Router, Utils) {
+	Router.init();
+	var f7 = new Framework7({
+		modalTitle: 'Contacts7',
+		swipePanel: 'left',
+        animateNavBackIcon: true
+	});
+    var mainView = f7.addView('.view-main', {
+        dynamicNavbar: true
+    });
+	return {
+		f7: f7,
+		mainView: mainView,
+		router: Router,
+		utils: Utils
+	};
+        // Initialize View          
+    var mainView = myApp.addView('.view')          
+            
+    // Load page from about.html file to main View:
+    mainView.router.loadPage('transfer.html');
+    mainView.router.loadPage('history.html');
+});
